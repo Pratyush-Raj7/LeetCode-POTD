ab5a1513d5416974baaf4e8b13365432053ea5b7
Pratyush-Raj7 <rpratyush163@gmail.com>
2026-01-12 16:43:02 +0530

    Jan 1-12 solution of POTD

---
+++ Jan 2026/02_ N-Repeated Element in Size 2N Array.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    int repeatedNTimes(vector<int>& nums) 
+    {
+        int n = nums.size() / 2;
+
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            int count = 0;
+            for (int j = 0; j < nums.size(); j++) 
+            {
+                if (nums[i] == nums[j])
+                    count++;
+            }
+            if (count == n)
+                return nums[i];
+        }
+        return -1;
+    }
+};
