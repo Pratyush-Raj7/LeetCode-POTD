6fc2a3e26180eab9ca2074589bba8dbc0bad23dc
Pratyush-Raj7 <rpratyush163@gmail.com>
2026-05-22 14:19:24 +0530

    POTD May 12th - May 22nd

---
+++ May 2026/15_Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+public:
+    int findMin(vector<int>& nums) 
+    {
+        int minVal = nums[0];
+        for (int i = 1; i < nums.size(); i++) 
+        {
+            if (nums[i] < minVal) minVal = nums[i];
+        }
+        return minVal;
+    }
+};
